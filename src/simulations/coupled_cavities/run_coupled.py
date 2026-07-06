f4706519ce8b1bcaff1c6d68feffbfb19a78ea44
Shahabedin Chatraee-Azizabadi <83550007+Shahabed@users.noreply.github.com>
2026-07-07 00:16:58 +0200

    Create run_coupled.py

---
+++ src/simulations/coupled_cavities/run_coupled.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from coupled_system import run_photon_statistics_sweep, calculate_spectrum
+
+if __name__ == "__main__":
+    print("Running coupled cavities examples...")
+    run_photon_statistics_sweep()
+    # Add spectrum examples here
