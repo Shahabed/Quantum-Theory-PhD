852d8cf1beab60fe42cb199ca682a76e3cf412ff
Shahabedin Chatraee-Azizabadi <83550007+Shahabed@users.noreply.github.com>
2026-07-07 01:08:02 +0200

    Create coupled_two_atoms.py

---
+++ src/scripts/coupled_two_atoms.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from src.models import *
+import numpy as np
+
+if __name__ == "__main__":
+    g1 = 0.1
+    Gamma_vec, n_avg, g2_vec = run_photon_sweep(g1=g1, n_target=2, system_type='atoms')
+    qsave(g2_vec, '../data/two_atoms_g2')
+    qsave(n_avg, '../data/two_atoms_n')
+    print("Two atoms sweep done.")
